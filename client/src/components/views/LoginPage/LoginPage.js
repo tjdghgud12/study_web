5e038f659c069cf40c525788e44b963d9228e69c
SeongHohyeong <seonghohyeong@Macintoshui-MacBookPro.local>
2021-11-24 16:05:27 +0900

    20211124-2

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { useDispatch } from  'react-redux';
 import { withRouter } from 'react-router-dom';
 import { loginUser } from '../../../_actions/user_action';
-import axios from 'axios';
 
 function LoginPage(props) {
     const dispatch = useDispatch();
